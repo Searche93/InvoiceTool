d2fbb5cce4daf2f44d19459b3da362d126a2056c
SergeZant <s.zant@syscon.nl>
2025-04-24 16:43:35 +0200

    Delete invoice test

---
+++ InvoiceTool.Application.Tests/Invoices/DeleteInvoiceTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using InvoiceTool.Application.Interfaces;
+using InvoiceTool.Application.UseCases.Invoices;
+using Moq;
+
+namespace InvoiceTool.Application.Tests.Invoices;
+
+public class DeleteInvoiceTests
+{
+    [Fact]
+    public async Task DeleteInvoice_Handle_InvoiceDeletedSuccessfull()
+    {
+        // Arrange
+        var mockedService = new Mock<IInvoiceService>();
+
+        mockedService
+            .Setup(s => s.DeleteAsync(It.IsAny<int>()))
+            .ReturnsAsync(true);
+
+        var useCase = new DeleteInvoice(mockedService.Object);
+
+        // Act
+        var result = await useCase.Execute(1);
+
+        // Assert
+        result.Should().Be(true);
+    }
+
+
+    [Fact]
+    public async Task DeleteInvoice_Handle_InvoiceDeletedUnSuccessfull()
+    {
+        // Arrange
+        var mockedService = new Mock<IInvoiceService>();
+
+        mockedService
+            .Setup(s => s.DeleteAsync(It.IsAny<int>()))
+            .ReturnsAsync(false);
+
+        var useCase = new DeleteInvoice(mockedService.Object);
+
+        // Act
+        var result = await useCase.Execute(1);
+
+        // Assert
+        result.Should().Be(false);
+    }
+
+
+    [Fact]
+    public async Task DeleteInvoice_Handle_ArgumentException()
+    {
+        // Arrange
+        var mockedService = new Mock<IInvoiceService>();
+
+        mockedService
+            .Setup(s => s.DeleteAsync(It.IsAny<int>()))
+            .ReturnsAsync(false);
+
+        var useCase = new DeleteInvoice(mockedService.Object);
+
+        // Act
+        var result = async () => await useCase.Execute(0);
+
+        // Assert
+        await result.Should().ThrowAsync<ArgumentException>();
+    }
+}
