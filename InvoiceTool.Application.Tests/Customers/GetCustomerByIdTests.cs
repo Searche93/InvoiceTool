3aa2053fda7752d5fa045b1d15c0a705fdbc7562
= <serge_zant@hotmail.com>
2025-11-06 15:06:53 +0100

    Tests

--- InvoiceTool.Application.Tests/Customers/GetCustomerByIdTests.cs
+++ InvoiceTool.Application.Tests/Customers/GetCustomerByIdTests.cs
@@ -1,9 +1,33 @@
-﻿namespace InvoiceTool.Application.Tests.Customers;
+﻿using InvoiceTool.Application.Interfaces;
+using InvoiceTool.Application.Models;
+using InvoiceTool.Application.UseCases.Customers;
+using Moq;
+
+namespace InvoiceTool.Application.Tests.Customers;
 
 public class GetCustomerByIdTests
 {
-    // Todo: Add tests for the following use cases:
+    [Fact]
+    public async Task Handle_ValidCustomer_GetCustomerSuccesfully()
+    {
+        var customerServiceMock = new Mock<ICustomerService>();
 
-    // GetCustomerById
-    // Fail on invalid id
+        var getCustomerByIdUseCase = new GetCustomerById(customerServiceMock.Object);
+
+        await getCustomerByIdUseCase.Execute(1);
+
+        customerServiceMock.Verify(x => x.GetAsync(1), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_InValidCustomer_GetCustomerUnSuccesfull()
+    {
+        var customerServiceMock = new Mock<ICustomerService>();
+
+        var getCustomerByIdUseCase = new GetCustomerById(customerServiceMock.Object);
+
+        var result = await getCustomerByIdUseCase.Execute(-1);
+
+        //await Assert.True(result is null);
+    }
 }
